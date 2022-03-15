40ca2c28ae45a7a306a6e4b7264de64169d45962
Vasiliy2020789 <89048929616@mail.ru>
2022-03-15 07:33:05 +0700

    Update Lesson_16.js

--- js/JawaScriptFPJ/Lesson_16.js
+++ js/JawaScriptFPJ/Lesson_16.js
@@ -12,7 +12,16 @@ console.log("// Пример 1 cвойства и методы форм");
 console.log("// Пример 1.1 получаем все формы document.forms ");
 console.log(document.forms);//возвращает живую коллекцию всех форм на странице
 console.log("// Пример 1.1 получаем конкретную форму document.forms [i] ");
-const formsArr = document.forms;
-const mainForm = document.forms[0];
+const formsArr = document.forms;//получение коллекции всех форм
+const mainForm = document.forms[0];//получение конкретной формы
 console.log(mainForm);
+const mainFormName = document.forms.main;//получение конкретной формы по атрибуту "name"
+console.log(mainFormName);
+console.log(document.querySelectorAll('form'));//получение НЕЖИВОЙ коллекци форм, не получает формы добавленые динамически
+console.log(document.forms);
 
+//Добавляем новую форму
+document.body.insertAdjacentHTML(
+	"beforeend",
+	'<form name="some" class="some-form" action="#"></form>'
+)
